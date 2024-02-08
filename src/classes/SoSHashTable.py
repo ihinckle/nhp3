22a5c73ed2496e29ec952e8944aeeb9cd7841405
isaac hinckley <ihinckle@gmail.com>
2024-02-07 22:30:10 -0700

    switched to use a tuple

--- src/classes/SoSHashTable.py
+++ src/classes/SoSHashTable.py
@@ -1,5 +1,5 @@
 class SoSHashTable:
-    __initial_value = {'key': -1, 'empty': 'start'}
+    __initial_value = (-1, 'start')
 
     def __init__(self, capacity):
         self.__capacity = capacity
@@ -11,12 +11,12 @@ class SoSHashTable:
             print('Table is full')
             return False
         bucket_index = hash(key) % self.__capacity
-        while self.table[bucket_index]['key'] != -1:
+        while self.table[bucket_index][0] != -1:
             if bucket_index == self.__capacity-1:
                 bucket_index = 0
             else:
                 bucket_index += 1
-        self.table[bucket_index] = item
+        self.table[bucket_index] = (key, item)
         self.__contains += 1
         return True
 
