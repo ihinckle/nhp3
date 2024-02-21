86546976c8fd3b5426cd380642d36a6093d16b65
Isaac Hinckley <ihinckle@gmail.com>
2024-02-20 17:48:10 -0700

    created SoSPackage.py

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
+import csv
 from src.classes.SoSHashTable import SoSHashTable
+from src.classes.SoSPackage import SoSPackage
 
-sosHashTable = SoSHashTable(10)
 
-sosHashTable.get(5)
+package_quantity = None
+packages = None
 
-sosHashTable.insert(1, {'key': 1})
-sosHashTable.insert(2, {'key': 2})
-sosHashTable.insert(3, {'key': 3})
-sosHashTable.insert(4, {'key': 4})
-sosHashTable.insert(5, {'key': 5})
-sosHashTable.insert(6, {'key': 6})
-sosHashTable.insert(7, {'key': 7})
-sosHashTable.insert(8, {'key': 8})
-sosHashTable.insert(9, {'key': 9})
-sosHashTable.insert(10, {'key': 10})
-sosHashTable.insert(11, {'key': 11})
-
-for x in sosHashTable.table:
-    print(x[1])
-
-for i in range(1, 11):
-    print(sosHashTable.get(i))
-
-print('tuple test')
-
-my_tuple = ('first', {'test': 'problem'})
-
-print(my_tuple[1])
-
-my_tuple[1]['test'] = 'changed'
-
-print(my_tuple[1])
+with open('resources/packages.csv') as packages_csv:
+    packages_reader = csv.reader(packages_csv)
+    package_quantity = sum(1 for row in packages_reader) - 1
+    packages = SoSHashTable(package_quantity)
+    for row in packages_reader:
+        if row[0] == 'ID':
+            continue
+        package = SoSPackage(row)
+        packages.insert(package.get_id(), package)
--- src/classes/SoSPackage.py
+++ src/classes/SoSPackage.py
@@ -1,3 +1,46 @@
-# class SoSPackage:
-#     def __init__(self):
+from enum import Enum
 
+
+class SoSPackage:
+    DELIVERY_STATUS = Enum('DeliveryStatus', ['AT_HUB', 'EN_ROUTE', 'DELIVERED'])
+
+    def __init__(self, package):
+        self.__delivery_status = self.DELIVERY_STATUS.AT_HUB.value
+        self.__id = package[0]
+        self.__address = package[1]
+        self.__city = package[2]
+        self.__state = package[3]
+        self.__zip_code = package[4]
+        self.__deadline = package[5]
+        self.__weight = package[6]
+        self.__special_note = package[7]
+
+    def get_id(self) -> str:
+        return self.__id
+
+    def get_address(self):
+        return self.__address
+
+    def get_city(self):
+        return self.__city
+
+    def get_state(self):
+        return self.__state
+
+    def get_zip(self):
+        return self.__zip_code
+
+    def get_deadline(self):
+        return self.__deadline
+
+    def get_weight(self):
+        return self.__weight
+
+    def get_special_note(self):
+        return self.__special_note
+
+    def get_delivery_status(self):
+        return self.__delivery_status
+
+    def set_delivery_status(self, delivery_status):
+        self.__delivery_status = delivery_status
